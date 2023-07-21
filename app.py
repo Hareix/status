4639ef4b0d4b12e1ed4c2eb55316affecdcb2e0e
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2023-07-21 17:44:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
-app = Flask(__name__)
+from animdl.core.cli.commands.grab import animdl_grab
+from aiohttp import web
+import json
 
-@app.route('/')
-def hello_world():
-    return 'cheems'
+async def start_web_client():
+    app = web.Application()
+    app.router.add_get('/', hello_world)
+    app.router.add_get('/get_streams', get_streams)
+    app.router.add_get('/get_anime', get_anime)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8000)  # Replace with your desired host and port
+    await site.start()
+    print("Web client started")
+    
+async def hello_world(request):
+    return web.Response(text='cheems')
 
+async def get_streams(request):
+    title = request.rel_url.query.get('name', '')
+    episode = request.rel_url.query.get('ep', '')
+    episode = 1 if episode is None else int(episode)
+    if not title:
+        return web.json_response({'error': 'Title and episode are required.'}, status=400)
+    try:
+        stream_data = animdl_grab(title, provider="haho", episode_number=episode)
+        streams_info = stream_data["anime_data"][0]["streams"]
+        streams = [{'url': stream_info['stream_url'], 'height': str(stream_info['quality'])} for stream_info in streams_info]
+        return web.json_response(streams)
+    except:
+        return web.json_response({'error': 'Data not found for the provided title and episode.'}, status=404)
 
-if __name__ == "__main__":
-    app.run()
+async def get_anime(request):
+    title = request.rel_url.query.get('name', '')
+    episode = request.rel_url.query.get('ep', '')
+    episode = 1 if episode is None else int(episode)
+    if not title:
+        return web.json_response({'error': 'Title and episode are required.'}, status=400)
+    try:
+       stream_data = animdl_grab(title, provider="gogoanime", episode_number=episode)
+       streams_info = stream_data["anime_data"][0]["streams"]
+       streams = [{'stream_url': stream_info['stream_url']} for stream_info in streams_info]
+       return web.json_response(streams)
+    except:
+        return web.json_response({'error': 'Data not found for the provided title and episode'}, status=404)
