c638200985de5a412ccb6320ac1187d86920b10d
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2023-07-21 18:30:05 +0530

    Update botstatus_teletips.py

--- botstatus_teletips.py
+++ botstatus_teletips.py
@@ -1,11 +1,84 @@
-from web_app import start_web_client
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 import asyncio
 import datetime
 import pytz
 import os
+from animdl.core.cli.commands.grab import animdl_grab
+from aiohttp import web
+import asyncio
+import json
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
+data_dict = {}
+mongo = MongoClient("mongodb+srv://sneha:sneha@cluster0.zhphhyu.mongodb.net/?retryWrites=true&w=majority")
+db = mongo.cheemsnsfk
+streamdb = db.animestream
+
+async def start_web_client():
+    cheb = web.Application()
+    cheb.router.add_get('/', hello_world)
+    cheb.router.add_get('/get_streams', get_streams)
+    cheb.router.add_get('/get_anime', get_anime)
+    runner = web.AppRunner(cheb)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8000)  # Replace with your desired host and port
+    await site.start()
+    print("Web client started")
+    await app.run(main_teletips())    
+    await asyncio.create_task(store_streams())
+    await asyncio.create_task(hen_videoss())
+
+
+async def hen_videoss():
+       while not await asyncio.sleep(3600):
+           await store_streams()
+           asyncio.create_task(hen_videoss())
+           break
+       asyncio.create_task(hen_videoss())
+
+        
+async def store_streams():
+      cursor = streamdb.find({})
+      async for document in cursor:
+        title = document.get('title')
+        episode = document.get('episode')
+        streams = document.get('streams')
+        key = (str(title), int(episode))
+        if key not in data_dict:
+            data_dict[key] = streams
+
+async def hello_world(request):
+    return web.Response(text='cheems')
+
+async def get_streams(request):
+    title = request.rel_url.query.get('name', '')
+    episode = request.rel_url.query.get('ep', '')
+    episode = 1 if episode is None else int(episode)
+    if not title:
+        return web.json_response({'error': 'Title and episode are required.'}, status=400)
+    try:
+        stream_data = animdl_grab(title, provider="haho", episode_number=episode)
+        streams_info = stream_data["anime_data"][0]["streams"]
+        streams = [{'url': stream_info['stream_url'], 'height': str(stream_info['quality'])} for stream_info in streams_info]
+        return web.json_response(streams)
+    except:
+        return web.json_response({'error': 'Data not found for the provided title and episode.'}, status=404)
+
+async def get_anime(request):
+    title = request.rel_url.query.get('name', '')
+    episode = request.rel_url.query.get('ep', '')
+    episode = 1 if episode is None else int(episode)
+    if not title:
+        return web.json_response({'error': 'Title and episode are required.'}, status=400)
+    try:
+       stream_data = animdl_grab(title, provider="gogoanime", episode_number=episode)
+       streams_info = stream_data["anime_data"][0]["streams"]
+       streams = [{'stream_url': stream_info['stream_url']} for stream_info in streams_info]
+       return web.json_response(streams)
+    except:
+        return web.json_response({'error': 'Data not found for the provided title and episode'}, status=404)
+        
 app = Client(
     api_id = int(os.environ.get("API_ID", "6")),
     api_hash = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e"),
@@ -55,7 +128,6 @@ async def main_teletips():
 
 async def run_app():
     await start_web_client()
-    await app.run(main_teletips())
 
 if __name__ == "__main__":
     asyncio.run(run_app())
