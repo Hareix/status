34d450c9dc2f91c721b26ad82e566ee75c1e2664
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-04-17 17:40:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/YukkiChatBot >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiChatBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiChatBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get it from my.telegram.org
+API_ID = int(getenv("API_ID", "6"))
+API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
+## Get it from @Botfather in Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN", "6842257955:AAFDEeJepF_yO7CJ9YV0Q90MEWuYRn9_S_8")
+
+# SUDO USERS
+SUDO_USER = list(
+    map(int, getenv("SUDO_USER", "5545068262").split())
+)  # Input type must be interger
+
+# ADMIN USERS
+ADMIN_USER = list(
+    map(int, getenv("ADMIN_USER", "5545068262").split())
+)  # Input type must be interger
+
+# Message to display when someone starts your bot
+PRIVATE_START_MESSAGE = getenv(
+    "PRIVATE_START_MESSAGE",
+    "Hello! Welcome to Cheems Support Bot, Ask Anything regarding our bots here.",
+)
